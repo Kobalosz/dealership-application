3e572eda661cea0d733f81b4a463cb1785812458
Kobalosz <imanuel.dartey@gmail.com>
2026-05-15 10:39:58 -0700

    Starter code init

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,10 +1,25 @@
 package com.pluralsight;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
+import com.pluralsight.application.DealershipApplication;
+import com.pluralsight.models.Dealership;
+import com.pluralsight.io.DealershipFileManager;
 
+import java.io.IOException;
 
+public class Main
+//    This is just to run the application!
+{
+    static void main()
+    {
+        try
+        {
+            Dealership dealership = DealershipFileManager.loadDealership();
+            DealershipApplication app = new DealershipApplication(dealership);
+            app.run();
+        }
+        catch(IOException e)
+        {
+            System.out.println("There was an error reading the data file.");
+        }
     }
 }
